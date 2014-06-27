7aac65a02be64d47b75ce884a5287cef3dfce93f
Lakshman Kumar Peethani <kumar.lakshman@gmail.com>
2014-06-27 16:53:48 +0530

    few minor changes

--- app.js
+++ app.js
@@ -10,15 +10,38 @@ var express = require('express')
 var port = process.env.PORT || 3000;
 
 // configure Express
-//noinspection JSValidateTypes
 app.configure(function() {
+
+    // Register ejs as .html. If we did
+    // not call this, we would need to
+    // name our views foo.ejs instead
+    // of foo.html. The __express method
+    // is simply a function that engines
+    // use to hook into the Express view
+    // system by default, so if we want
+    // to change "foo.ejs" to "foo.html"
+    // we simply pass _any_ function, in this
+    // case `ejs.__express`.
+
+    app.engine('.html', require('ejs').__express);
+
+    // Optional since express defaults to CWD/views
+
     app.set('views', __dirname + '/views');
-	app.engine('.html', require('ejs').__express);
+
+    // Without this you would need to
+    // supply the extension to res.render()
+    // ex: res.render('users.html').
     app.set('view engine', 'html');
+
 	app.use(express.logger('dev'));
 	app.use(express.cookieParser());
-	app.use(connect.bodyParser());
-	app.use(connect.methodOverride());
+
+    // Following two lines are used instead of the express.bodyParser()
+    app.use(express.urlencoded());
+    app.use(express.json());
+
+	//app.use(connect.methodOverride());
 	app.use(express.session({ secret: 'keyboard cat' }));
 
 	// Remember Me middleware
