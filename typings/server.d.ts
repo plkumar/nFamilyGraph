dd9eb3424889f5c01024591eb31cd5aa851dd020
Lakshman Kumar Peethani <kumar.lakshman@gmail.com>
2014-07-06 12:52:41 +0530

    same as previous check-in

---
+++ typings/server.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="express/express.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="mongodb/mongodb.d.ts" />
+/// <reference path="mongoose/mongoose.d.ts" />
+/// <reference path="passport/passport.d.ts" />
+/// <reference path="passport-facebook/passport-facebook.d.ts" />
+/// <reference path="passport-local/passport-local.d.ts" />
